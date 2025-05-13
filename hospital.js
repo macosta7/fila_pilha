4faed6e76a288f12ad22b3fff1e826f3b52db43a
macosta7 <marcela.costa@sou.inteli.edu.br>
2025-05-12 21:35:38 -0300

    docs: adiciona comentarios no codigo

--- hospital.js
+++ hospital.js
@@ -1,68 +1,70 @@
 class Fila {
     constructor() {
-        this.pacientes = [];
+        this.pacientes = []; //Fila de pacientes
     }
 
-    adicionarPaciente(paciente) {
-        this.pacientes.push(paciente);
-        console.log(`📥 ${paciente} adicionado à fila`);
+    adicionarPaciente(paciente) { //Adiciona um paciente à fila
+        this.pacientes.push(paciente); //Adiciona o paciente no final da fila
+        console.log(`📥 ${paciente} adicionado à fila`); //Mostra que o paciente foi adicionado
     }
 
-    proximoPaciente() {
-        return this.pacientes[0];
+    proximoPaciente() { //Metodo para mostrar o proximo paciente
+        return this.pacientes[0]; //Mostra o primeiro paciente da fila
     }
 
-    removerPaciente() {
-        if(this.estaVazia()) {
-            console.log(`⚠️ A fila está vazia. Nenhum paciente para remover.`);
+    removerPaciente() { //Metodo para remover o paciente da fila(atender)
+        if(this.estaVazia()) { //Verifica se a fila está vazia
+            console.log(`⚠️ A fila está vazia. Nenhum paciente para remover.`); //Caso esteja vazia, mostra mensagem
             return null;
         }
 
-        const removido = this.pacientes.shift();
-        console.log(`🚪 ${removido} saiu da fila.`);
-        return removido;
+        const removido = this.pacientes.shift(); //Primeiro remove o paciente da fila
+        console.log(`🚪 ${removido} saiu da fila.`); //Mostra que o paciente saiu da fila
+        return removido; 
     }
 
-    estaVazia() {
-        return this.pacientes.length === 0;
+    estaVazia() { //Metodo para verificar se a fila está vazia
+        return this.pacientes.length === 0; //Verifica se o tamanho da fila é 0
     }
 
-    todosPacientes() {
-        console.log('🚶‍♂️ Fila atual:', this.pacientes.join(' -> ') || 'vazia');
+    todosPacientes() { //Metodo para mostrar todos os pacientes da fila
+        console.log('🚶‍♂️ Fila atual:', this.pacientes.join(' -> ') || 'vazia'); 
     }
 }
 
 class Pilha {
     constructor() {
-        this.prontuarios = [];
+        this.prontuarios = []; //Pilha dos prontuários
     }
 
-    adicionarProntuario(paciente){
-        this.prontuarios.push(paciente);
-        console.log(`✔️ ${paciente} adicionado ao prontuário`);
+    adicionarProntuario(paciente){ //Adiciona o prontuário do paciente que foi removido da fila
+        this.prontuarios.push(paciente); //Adiciona o prontuário no final da pilha
+        console.log(`✔️ ${paciente} adicionado ao prontuário`); //Mostra que o prontuário foi adcionado
     }
 
-    todosProntuarios() {
-        console.log('📋 Prontuários:', this.prontuarios.slice().reverse().join(' <- ') || 'vazia');
+    todosProntuarios() { //Metodo para mostrar todos os prontuários
+        console.log('📋 Prontuários:', this.prontuarios.slice().reverse().join(' <- ') || 'vazia'); //Mostra os prontuários
     }
 }
 
-// DEMONSTRAÇÃO
-
+// Cria instâncias de Fila e Pilha
 const filaPacientes = new Fila();
 const pilhaProntuarios = new Pilha();
 
 console.log('------------------------------------');
 
+//Adiciona os pacientes na fila
 const pacientes = ['Marcela', 'Arthur', 'Rogerio', 'Rose', 'Marcelo'];
 pacientes.forEach(p => filaPacientes.adicionarPaciente(p));
 
 console.log('------------------------------------');
 
+//Mostra o próximo paciente
 console.log(`🔍 Próximo paciente: ${filaPacientes.proximoPaciente()}`);
 
 console.log('------------------------------------');
 
+//Atende os dois primeiros pacientes e adiciona os prontuários deles na pilha
 for (let i = 0; i < 2; i++) {
     const atendido = filaPacientes.removerPaciente();
     if(atendido) {
@@ -72,6 +74,7 @@ for (let i = 0; i < 2; i++) {
 
 console.log('------------------------------------');
 
+//Mostra a fila de pacientes e os prontuários
 filaPacientes.todosPacientes();
 pilhaProntuarios.todosProntuarios();
 
